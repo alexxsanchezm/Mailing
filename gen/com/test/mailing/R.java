d0116b9581e3063f97b92f7298290ada74a09481
Alex <alexxsanchezm@gmail.com>
2014-04-27 22:16:00 -0500

    App for testing propouses
    This app send emails without an android client.

--- gen/com/test/mailing/R.java
+++ gen/com/test/mailing/R.java
@@ -1078,6 +1078,9 @@ containing a value of this type.
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
         public static final int ic_launcher=0x7f020057;
+        public static final int shape_1=0x7f020058;
+        public static final int shape_2=0x7f020059;
+        public static final int sltr_btn_1=0x7f02005a;
     }
     public static final class id {
         public static final int action_bar=0x7f05001c;
@@ -1091,10 +1094,11 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003d;
+        public static final int action_settings=0x7f050044;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
+        public static final int btn_send=0x7f050043;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int container=0x7f05003c;
@@ -1104,6 +1108,9 @@ containing a value of this type.
         public static final int dropdown=0x7f05000f;
         public static final int edit_query=0x7f050030;
         public static final int end=0x7f050013;
+        public static final int et_contact=0x7f05003e;
+        public static final int et_content=0x7f050042;
+        public static final int et_subject=0x7f050040;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
         public static final int home=0x7f050014;
@@ -1137,6 +1144,9 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
+        public static final int textView1=0x7f05003d;
+        public static final int textView2=0x7f05003f;
+        public static final int textView3=0x7f050041;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
--- src/com/test/mailing/HomeActivity.java
+++ src/com/test/mailing/HomeActivity.java
@@ -1,14 +1,33 @@
 package com.test.mailing;
 
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import android.os.Build;
 
 public class HomeActivity extends ActionBarActivity {
@@ -47,8 +66,13 @@ public class HomeActivity extends ActionBarActivity {
 	/**
 	 * A placeholder fragment containing a simple view.
 	 */
-	public static class PlaceholderFragment extends Fragment {
+	public static class PlaceholderFragment extends Fragment implements OnTouchListener {
 
+		Button btn;
+		EditText et_conntact;
+		EditText et_subject;
+		EditText et_message;
+		
 		public PlaceholderFragment() {
 		}
 
@@ -59,6 +83,116 @@ public class HomeActivity extends ActionBarActivity {
 					false);
 			return rootView;
 		}
+		
+		@Override
+		public void onActivityCreated(Bundle savedInstanceState){
+			super.onActivityCreated(savedInstanceState);
+			btn = (Button)PlaceholderFragment.this.getView().findViewById(R.id.btn_send);
+			
+			et_conntact = (EditText)PlaceholderFragment.this.getView().findViewById(R.id.et_contact);
+			et_subject = (EditText)PlaceholderFragment.this.getView().findViewById(R.id.et_subject);
+			et_message = (EditText)PlaceholderFragment.this.getView().findViewById(R.id.et_content);
+			
+			btn.setOnTouchListener(this);
+		}
+
+		@Override
+		public boolean onTouch(View v, MotionEvent event) {
+			if(btn.getId() == v.getId()){
+				if(event.getAction() == MotionEvent.ACTION_UP){
+					
+					String strContact = et_conntact.getText().toString();
+					String strsubject = et_subject.getText().toString();
+					String strMessage = et_message.getText().toString();
+					
+					if(strContact.equals("")){
+						Toast.makeText(this.getActivity(), "Falto Destinatario", Toast.LENGTH_SHORT).show();
+						return true;
+					}
+					if(strsubject.equals("")){
+						Toast.makeText(this.getActivity(), "Falto Asunto.", Toast.LENGTH_SHORT).show();
+						return true;
+					}
+					if(strMessage.equals("")){
+						Toast.makeText(this.getActivity(), "Mensaje vacio.", Toast.LENGTH_SHORT).show();
+					}
+					
+					performSendMail(strContact, strsubject, strMessage);
+					return true;
+				}
+			}
+			return false;
+		}
+		
+		private void performSendMail(String email, String subject, String messageBody){
+			Log.d("Hello", "World");
+			Session session = createSessionObject();
+			
+			Message msg = createMessage(email, subject, messageBody, session);
+			new SendMailTask().execute(msg);
+		}
+
+		private Message createMessage(String email, String subject,
+				String messageBody, Session session) {
+			Message msg = new MimeMessage(session);
+			try {
+				msg.setFrom(new InternetAddress("alexxsanchezm@gmail.com", "Alex Sanchez"));
+				msg.addRecipient(Message.RecipientType.TO, new InternetAddress(email, email));
+				msg.setSubject(subject);
+				msg.setText(messageBody);
+			
+			} catch (UnsupportedEncodingException | MessagingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			return msg;
+		}
+
+		private Session createSessionObject() {
+			Properties properties = new Properties();
+			properties.put("mail.smtp.auth", "true");
+			properties.put("mail.smtp.starttls.enable", "true");
+			properties.put("mail.smtp.host", "smtp.gmail.com");
+			properties.put("mail.smtp.port", "587");
+			
+			
+			return Session.getInstance(properties, new javax.mail.Authenticator() {
+	            protected PasswordAuthentication getPasswordAuthentication() {
+	                return new PasswordAuthentication("alexxsanchezm@gmail.com", "albk:1989");
+	            }
+	        });
+		}
+	
+		private class SendMailTask extends AsyncTask<Message, Void, Void>{
+
+			@Override
+			protected void onPreExecute(){
+				super.onPreExecute();
+				Log.d("Hello", "Sending Mail......");
+			}
+			
+			@Override
+			protected Void doInBackground(Message... msgs) {
+				try {
+					Transport.send(msgs[0]);
+				} catch (MessagingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				return null;
+			}
+			
+			@Override
+			protected void onPostExecute(Void aVoid){
+				super.onPostExecute(aVoid);
+				Log.d("Hello", "Mail Sent, I Guess.");
+			}
+			
+		}
+	
 	}
+	
+	
 
 }
